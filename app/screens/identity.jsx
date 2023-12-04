52ead637163b72ac33f514ff0881d086ac182aa8
Javed Chaudhary <jachoudhary@oneshield.com>
2023-12-04 22:26:46 +0530

    identity section completed

--- app/screens/identity.jsx
+++ app/screens/identity.jsx
@@ -8,14 +8,14 @@ export default function Identity(){
         <section className="identitysection">
         <h1 className='identityTitle'>Explore what sets an Nirvana residence apart</h1>
             <div className="identityContentcontainer">
-            <div className="identityImage">
-              <Image src='/h1.jpg' alt='villa image' fill={true} objectFit='cover' />
-            </div>
+                 <div className="identityImage">
+                <Image src='/h2.jpg' alt='villa image' layout='fill' objectFit='cover'/>
+                </div>
             <div className="identyConetent">
-                <h2>Ideal location</h2>
-                <h2>Elegant Property</h2>
-                <h2>Commitment to the best guest experience</h2>
-                <h2>Best in class amenities</h2>
+                <h2><div className="identityicon"><Image src='/location_black.png' alt='location icon' layout='fill' objectFit='contain'/></div>Ideal location</h2>
+                <h2><div className="identityicon"><Image src='/property.png' alt='property icon' layout='fill' objectFit='contain'/></div>Elegant Property</h2>
+                <h2><div className="identityicon"><Image src='/customer.png' alt='customer icon' layout='fill' objectFit='contain'/></div>Commitment to the best guest experience</h2>
+                <h2><div className="identityicon"><Image src='/high-quality.png' alt='service icon' layout='fill' objectFit='contain'/></div>Best in class amenities</h2>
             </div>
             </div>
         </section>
