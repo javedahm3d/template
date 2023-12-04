f93d78cb26a6bf736020f22c6040dbfcabc28d8c
Tanmay-Nivaskar <tanmaynivaskar64980@gmail.com>
2023-12-04 14:27:38 +0530

    Slider :- Made new slider

--- app/page.js
+++ app/page.js
@@ -1,12 +1,16 @@
+"use client";
+
 import servicesscroll from "./components/servicesInfiniteScroll"
 import aboutUs from "./screens/about"
 import heroPage from "./screens/hero"
+import slider from './components/slider'
 
 export default function Home() {
   return (
     <main className="">
       {heroPage()}
       {aboutUs()}
+      {slider()}
       {servicesscroll()}
     </main>
   )
