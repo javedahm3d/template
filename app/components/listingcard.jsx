68a7264ab72ca4863319ca8d4e8c790f7ba626f6
Javed Chaudhary <jachoudhary@oneshield.com>
2023-12-12 15:51:50 +0530

    listing page responsive

--- app/components/listingcard.jsx
+++ app/components/listingcard.jsx
@@ -1,17 +1,4 @@
 import '@/style/listingcard.scss'
-import Image from 'next/image'
-
-
-export default function listingcard({image, name , price , location , rooms , decription}){
-    return(
-        <div className="listingcard">
-            <div className="experienceImageContainer">
-            <Image src={image} alt='experince Card image' fill={true} objectFit='cover' />
-            </div>
-            <p className='experinceTitle'>{title}</p>
-        </div>
-    )
-}import '@/style/listingcard.scss'
 import { TfiLocationPin } from "react-icons/tfi";
 import { FaIndianRupeeSign } from "react-icons/fa6";
 import { MdOutlineBedroomChild } from "react-icons/md";
--- app/listing/page.jsx
+++ app/listing/page.jsx
@@ -1,18 +1,74 @@
 "use client";
 import footer from "../components/footer";
 import Navbar from "../components/navbar";
-import "@/style/listing.css"
+import "@/style/listing.scss";
 import categorybar from "../components/categorybar";
+import Listingcard from "../components/listingcard";
 
 export default function Home() {
   return (
-    <main className="">
-      <Navbar setNavbarvalue={true} />      
+    <main>
+      <Navbar setNavbarvalue={true} />
       <div className="identityandReviewsectionbg pt-20">
-      {categorybar()}
-      <div className="h-4"></div>
-      {footer()}
+        {categorybar()}
+        <div className="h-4"></div>
+
+        <div className="listingpage">
+          <div className="listingpagecontainer">
+            {data.map((d, index) => (
+              <Listingcard
+                image={d.image}
+                name={d.name}
+                price={d.price}
+                location={d.location}
+                rooms={d.rooms}
+                labels={d.labels}
+              />
+            ))}
+
+            {/* {listingcard()} */}
+          </div>
+        </div>
+
+        {footer()}
       </div>
     </main>
-  )
+  );
 }
+
+// {image, name , price , location , rooms}
+
+const data = [
+  {
+    name: "Luxury pool villa",
+    image: "/h1.jpg",
+    price: "25000",
+    location: "Curtorim, Goa",
+    rooms: "3",
+    labels: ["mountain", "beach"],
+  },
+  {
+    name: "Luxury pool villa",
+    image: "/h1.jpg",
+    price: "25000",
+    location: "Curtorim, Goa",
+    rooms: "3",
+    labels: ["mountain", "beach"],
+  },
+  {
+    name: "Luxury pool villa",
+    image: "/h1.jpg",
+    price: "25000",
+    location: "Curtorim, Goa",
+    rooms: "3",
+    labels: ["mountain", "beach"],
+  },
+  {
+    name: "Luxury pool villa",
+    image: "/h1.jpg",
+    price: "25000",
+    location: "Curtorim, Goa",
+    rooms: "3",
+    labels: ["mountain", "beach"],
+  },
+];
