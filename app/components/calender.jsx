5347b96950a7acf78875485eaf11f928369452d2
Javed Chaudhary <jachoudhary@oneshield.com>
2024-01-13 23:30:49 +0530

    date error fixed

--- app/components/calender.jsx
+++ app/components/calender.jsx
@@ -18,7 +18,10 @@ export default function calendar() {
   ]);
 
   const formatToYYYYMMDD = (date) => {
-    return date.toISOString().split('T')[0];
+    const year = date.getFullYear();
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const day = date.getDate().toString().padStart(2, '0');
+    return `${year}-${month}-${day}`;
   };
 
   const onSelectDate = (ranges) => {
